56271128316cb928bf807ba7d656a4db38ccaaa5
rakati <ouhaddounouredine@gmail.com>
2024-04-19 20:44:05 +0100

    fix werkzeug version problem in flask 2.1.0 version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # install flask package using pip
+package { 'werkzeug':
+    ensure   => '2.2.2',
+    provider => 'pip3',
+}
+
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+    require  => Package['werkzeug']
 }
