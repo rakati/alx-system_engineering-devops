0cc24989b9c9a319b049a2502ecf77041c2ea513
rakati <ouhaddounouredine@gmail.com>
2024-06-04 20:02:54 +0100

    Improve code style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-'''Script for requesting data from Reddit API'''
+"""
+Script 00:
+Script for requesting data from Reddit API
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''return the number of subscribers for the given subreddit'''
-    resp = requests.get("https://www.reddit.com/r/{}/about.json"
-                        .format(subreddit),
-                        headers={"User-Agent": "ALX-API-Advanced-Script"},
-                        allow_redirects=False)
-    if resp.status_code < 300:
-        return resp.json()['data']['subscribers']
-    return 0
+    """
+    Query the number of subscribers (active and non active user) for the given
+    subreddit.
+
+    Parameters:
+    -----------
+        - subreddit (str): Name of the subreddit to look for
+    Returns:
+    --------
+        - Number of subscribers to the subreddit or 0 if
+          the subreddit is invalid.
+    """
+    resp = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "ALX-API-Advanced-Script"},
+        allow_redirects=False
+    )
+
+    return 0 if resp.status_code != 200 else resp.json()['data']['subscribers']
