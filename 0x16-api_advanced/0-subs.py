4a41f858dcae5bc2e86fe10c3ec165269eb326ca
rakati <ouhaddounouredine@gmail.com>
2024-06-04 19:45:36 +0100

    create a script that request number of subscribers of subreddit from reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Script for requesting data from Reddit API'''
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''return the number of subscribers for the given subreddit'''
+    resp = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers={"User-Agent": "ALX-API-Advanced-Script"},
+                        allow_redirects=False)
+    if resp.status_code == 200:
+        return resp.json()['data']['subscribers']
+    return None
